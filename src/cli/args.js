4fc73433ec7891e1958c44e3020167f33c3baa1c
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-24 15:10:41 +0100

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv
+    .slice(2)
+    .reduce(
+      (acc, el, ind, arr) =>
+        el.startsWith("--")
+          ? [...acc, `${el.slice(2)} is ${arr[ind + 1]}`]
+          : acc,
+      []
+    )
+    .join(", ");
+  console.log(args);
 };
 
 parseArgs();
