6e74d991b9636f584ab3e217186e2c31deb65748
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-24 13:39:33 +0100

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = path.resolve("src/fs");
+const pathToFolder = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(pathToFolder);
+    console.log(files);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
