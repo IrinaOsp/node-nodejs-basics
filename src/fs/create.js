bab38f501111dea8c51a144df8540d9eefc0fdda
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-24 10:23:28 +0100

    feat: create task solved

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = path.resolve("src/fs");
+const pathToFile = path.join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(
+      pathToFile,
+      "I am fresh and young",
+      { flag: "wx" },
+      (err) => {
+        if (err) throw new Error("FS operation failed");
+      }
+    );
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
