e44a5635acd905a7e94153e3fbddf5c2ea53c31d
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-25 09:37:28 +0100

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import path from "path";
+import fs from "fs/promises";
+import crypto from "crypto";
+
+const __dirname = path.resolve("src/hash");
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const data = await fs.readFile(pathToFile, "utf-8");
+  const hash = crypto.createHash("sha256").update(data).digest("hex");
+  console.log(hash);
 };
 
 await calculateHash();
