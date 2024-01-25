f584095e33dd0dbf0281cade70c1b765dc2ea590
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-25 09:58:27 +0100

    feat: add write solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import path from "path";
+import { createWriteStream } from "fs";
+
+const __dirname = path.resolve("src/streams");
+const pathToFile = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(pathToFile);
+  process.stdin.pipe(writeStream);
 };
 
 await write();
