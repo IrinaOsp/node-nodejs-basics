0b7397c480b0e4f9033c51c4ff91209af47781a3
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-29 10:36:34 +0100

    feat: worker threads solved

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import path from "path";
+import { Worker, isMainThread } from "worker_threads";
+import os from "os";
+
+const __dirname = path.resolve("src/wt");
+const pathToWorker = path.join(__dirname, "worker.js");
+const StartingNumber = 10;
+
+const createWorker = (num) => {
+  return new Promise((resolve, _) => {
+    const worker = new Worker(pathToWorker, { workerData: num });
+    worker.on("message", (result) =>
+      resolve({ status: "resolved", data: result })
+    );
+    worker.on("error", () => resolve({ status: "error", data: null }));
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  if (isMainThread) {
+    const coresNumber = os.cpus().length;
+    const results = [];
+
+    for (let i = 0; i < coresNumber; i++) {
+      const result = await createWorker(StartingNumber + i);
+      results.push(result);
+    }
+    console.log(results);
+  }
 };
 
 await performCalculations();
